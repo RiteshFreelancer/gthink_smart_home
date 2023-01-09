ac90fef2f50f4bfc1c3894e045aa83332dccc04a
unknown <ritesh.freelance01@gmail.com>
2023-01-09 14:39:01 +0530

    [ADDED] some files added

--- gskill/wsgi.py
+++ gskill/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for smart_home project.
+WSGI config for gskill project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_home.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gskill.settings')
 
 application = get_wsgi_application()
