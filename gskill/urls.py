9ec2a0bddaf3baa84b6f0c090df72f575218edf7
unknown <ritesh.freelance01@gmail.com>
2023-01-15 03:07:07 +0530

    [UPDATES] Updates

--- gskill/urls.py
+++ gskill/urls.py
@@ -5,7 +5,7 @@ from accounts.views import UserList, UserDetails, GroupList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('o/', include('adminoauth2_provider.urls', namespace='oauth2_provider')),
     # path('users/', UserList.as_view()),
     # path('users/<pk>/', UserDetails.as_view()),
     # path('groups/', GroupList.as_view()),
